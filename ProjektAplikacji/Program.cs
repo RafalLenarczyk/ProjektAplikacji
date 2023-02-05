586948b6fca520c24d5e199bfd2eda0b88689737
RafalLenarczyk <r-lenarczyk@wp.pl>
2023-02-05 20:06:31 +0100

    Dzień 4 - zadanie domowe

---
+++ ProjektAplikacji/Program.cs
@@ -0,0 +1,17 @@
+﻿string name = "Ewa";
+bool isWoman = true;
+int age = 33;
+
+if (isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (isWoman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
